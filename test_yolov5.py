1619934423ecc1946b387fcabfc7f4f3f57e5263
StaceyXC <982753946@qq.com>
2022-11-04 18:20:49 +0800

    暂存 还没开始

---
+++ test_yolov5.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import cv2
+from postprocess import postprocess
+import jiami
+
+from hobot_dnn import pyeasy_dnn as dnn
+
+
+def bgr2nv12_opencv(image):
+    height, width = image.shape[0], image.shape[1]
+    area = height * width
+    yuv420p = cv2.cvtColor(image, cv2.COLOR_RGB2YUV_I420).reshape((area * 3 // 2,))
+    y = yuv420p[:area]
+    uv_planar = yuv420p[area:].reshape((2, area // 4))
+    uv_packed = uv_planar.transpose((1, 0)).reshape((area // 2,))
+
+    nv12 = np.zeros_like(yuv420p)
+    nv12[:height * width] = y
+    nv12[height * width:] = uv_packed
+    return nv12
+
+
+def get_hw(pro):
+    if pro.layout == "NCHW":
+        return pro.shape[2], pro.shape[3]
+    else:
+        return pro.shape[1], pro.shape[2]
+
+
+def print_properties(pro):
+    print("tensor type:", pro.tensor_type)
+    print("data type:", pro.dtype)
+    print("layout:", pro.layout)
+    print("shape:", pro.shape)
+
+
+if __name__ == '__main__':
+    #加载模型
+    models = dnn.load('../models/yolov5s_672x672_nv12.bin')
+    # 打印输入 tensor 的属性
+    print_properties(models[0].inputs[0].properties)
+    # 打印输出 tensor 的属性
+    print(len(models[0].outputs))
+    for output in models[0].outputs:
+        print_properties(output.properties)
+    #读取图像
+    img_file = cv2.imread('./kite.jpg')
+    h, w = get_hw(models[0].inputs[0].properties)
+    des_dim = (w, h)
+    resized_data = cv2.resize(img_file, des_dim, interpolation=cv2.INTER_AREA)
+    #格式转换
+    nv12_data = bgr2nv12_opencv(resized_data)
+    #模型推理
+    outputs = models[0].forward(nv12_data)
+    #检测框合成
+    prediction_bbox = postprocess(outputs, model_hw_shape=(672, 672), origin_image=img_file)
+    print(prediction_bbox)
